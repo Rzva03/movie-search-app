b50e60df363c55788446abd035acd45da992ef39
Rzva03 <avictor.zrodriguez@gmail.com>
2023-10-20 14:54:37 -0600

    feat: add new styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
+
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+
 import './main.css';
+import './assets/css/global.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
