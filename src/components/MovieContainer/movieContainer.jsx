ebb6f05fd7dbb8f9edacaea14cba6ac0900bfc53
Rzva03 <avictor.zrodriguez@gmail.com>
2023-10-23 11:38:08 -0600

    feat: add component for no data

--- src/components/MovieContainer/movieContainer.jsx
+++ src/components/MovieContainer/movieContainer.jsx
@@ -1,31 +1,39 @@
 import PropTypes from 'prop-types';
 import MovieCard from '../MovieCard/movieCard';
+import Skeleton from '../Skeleton/skeleton';
+import NoData from '../NoData/noData';
 
 const MovieContainer = (props) => {
     const {
-        movies
+        movies,
+        isLoading
     } = props;
 
     if (movies === undefined) {
-        return
+        return <NoData />
     }
 
     return (
-        <main className='w-full grid grid-cols-4 gap-6'>
-            {movies.map((movie) => (
-                <MovieCard
-                    key={`movie__${movie.imdbID}`}
-                    title={movie.Title}
-                    poster={movie.Poster}
-                    year={movie.Year}
-                />
-            ))}
+        <main className='w-full grid grid-cols-1 md:grid-cols-5 gap-6'>
+            {isLoading ? (
+                <Skeleton />
+            ) : (
+                movies.map((movie) => (
+                    <MovieCard
+                        key={`movie__${movie.imdbID}`}
+                        title={movie.Title}
+                        poster={movie.Poster}
+                        year={movie.Year}
+                    />
+                ))
+            )}
         </main>
     );
 }
 
 MovieContainer.propTypes = {
-    movies: PropTypes.array
+    movies: PropTypes.array,
+    isLoading: PropTypes.bool
 }
 
 export default MovieContainer;
